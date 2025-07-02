5a2dad6d9c2c9032936e03f35b1d0801f798a73d
Kepa Aburto <184770699+keps21678@users.noreply.github.com>
2025-07-02 17:50:06 +0200

    Updates page title to reflect app purpose
    Changes the HTML title to "Gestión de AA.VV" for better clarity and alignment with the application's intended function.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Prueba01</title>
+        <title>Gestión de AA.VV</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
